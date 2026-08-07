d90ebfb2e60f8526522dc3339f4bcfa26796018c
Muhammad Atif <atifwazir45@gmail.com>
2026-08-07 12:49:56 +0500

    Add basic decision-making to Agent

--- agent/agent.py
+++ agent/agent.py
@@ -7,4 +7,27 @@ class Agent:
         if self.convo_list:
             return self.convo_list[-1] 
         else:
-            return "Convo list is empty."              
+            return "Convo list is empty."  
+    def check_intent(self):
+        last_msg = self.get_last_msg().lower().split()
+        if last_msg != "Convo list is empty.":
+            for word in last_msg:
+                if word == "hi" or word == "hello":
+                    return "Greetings"
+                elif word == "order" or word == "payment":
+                    return "Order"
+                elif word == "refund" or word == "return":
+                    return "Return"
+            return "Nothing Understandable"
+        else:
+            return last_msg
+    def decide_action(self):
+        intent = self.check_intent()
+        if intent == "Greetings":
+            return "Handle Greeting"
+        elif intent == "Order":
+            return "Handle Order"   
+        elif intent == "Return":
+            return "Handle return"
+        else:
+            return "No decision"
