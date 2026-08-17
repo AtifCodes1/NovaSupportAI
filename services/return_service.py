fc43bf187f639a38194d4e33c9c3002d00314cbf
Muhammad Atif <atifwazir45@gmail.com>
2026-08-17 10:39:00 +0500

    connect return service to mock backend

--- services/return_service.py
+++ services/return_service.py
@@ -1,17 +1,9 @@
+from api.client import APIClient
 class ReturnService:
     def __init__(self):
-        self.orders = {
-            "1001" :{
-                   "return_eligible":True,
-                   "return_status":"Not_requested" },
-            "1002" :{
-                "return_eligible":False,
-                "return_status":"Not_eligible"}}
+        self.api_client = APIClient("http://127.0.0.1:8001")
     def process_return(self,ordernumber):
-        if ordernumber not in self.orders:
-            return None
-        order = self.orders[ordernumber]
-        if not order["return_eligible"]:
-            return order
-        order["return_status"]= "Requested"
-        return order
+        response = self.api_client.get(f"/returns/{ordernumber}")
+        if response.status_code == 200:
+            return response.json()
+        return None
